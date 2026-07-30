20be963482215033cc935cf03e3c3fa2bb4fa18c
Kawshik Ahmed Ornob <97808635+kawshik-ornob8@users.noreply.github.com>
2026-07-30 20:17:49 +0600

    Update site.js chnage to ${escapeHtml(profile.github)}

--- assets/site.js
+++ assets/site.js
@@ -1074,7 +1074,7 @@ function renderFooter() {
       </div>
     </div>
     <!-- Developer attribution: Rajib Khan -->
-    <p class="developer-credit footer-credit-final">Developed by <a href="${escapeHtml(profile.github)}" target="_blank" rel="noreferrer">Kawshik Ahmed Ornob</a></p>
+    <p class="developer-credit footer-credit-final">Developed by <a href="kawshik.dev" target="_blank" rel="noreferrer">Kawshik Ahmed Ornob</a></p>
   `;
 }
 
